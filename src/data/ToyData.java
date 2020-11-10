2b82852cf223d5f03de2b23b7dadf415c5866ee6
quarantinoboy <zoccer1231@gmail.com>
2020-11-10 17:41:52 +0700

    create 10 parcels

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -1,7 +1,9 @@
 package data;
 
 import base.Bike;
+import base.Location;
 import base.Parcel;
+import base.ParcelStatus;
 import base.Person;
 
 public class ToyData {
@@ -11,8 +13,6 @@ public class ToyData {
         bikes[0] = new Bike("nmax",155);
         bikes[1] = new Bike("xmax",300);
         bikes[2] = new Bike("pcx",150);
-        // your code
-
         return bikes;
     }
 
@@ -26,6 +26,16 @@ public class ToyData {
 
     public static Parcel[] getParcels() {
         Parcel[] parcels = new Parcel[10];
+        parcels[0] = new Parcel(12, ParcelStatus.READY_TO_SHIP, new Location(15.8441,16.25454));
+        parcels[1] = new Parcel(24, ParcelStatus.DELIVERED, new Location(13.02534,16.1464)); 
+        parcels[2] = new Parcel(63, ParcelStatus.DELIVERED, new Location(15.5218,16.5158)); 
+        parcels[3] = new Parcel(40, ParcelStatus.SHIPPING, new Location(15.15345,16.5418));
+        parcels[4] = new Parcel(11, ParcelStatus.SHIPPING, new Location(15.81551,16.8315));
+        parcels[5] = new Parcel(22, ParcelStatus.DELIVERED, new Location(15.85645,16.8202)); 
+        parcels[6] = new Parcel(13, ParcelStatus.READY_TO_SHIP, new Location(15.0121,16.2122)); 
+        parcels[7] = new Parcel(19, ParcelStatus.DELIVERED, new Location(15.1231,16.2251)); 
+        parcels[8] = new Parcel(24, ParcelStatus.READY_TO_SHIP, new Location(15.0215,16.0216)); 
+        parcels[9] = new Parcel(26, ParcelStatus.READY_TO_SHIP, new Location(15.3156,16.5644)); 
         return parcels;
     }
 
